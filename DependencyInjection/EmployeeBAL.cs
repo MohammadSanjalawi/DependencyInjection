3e199cf44551d7cf111123a26b5bff19cd53f4d3
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-05-28 03:33:37 +0300

    Add project files.

---
+++ DependencyInjection/EmployeeBAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDependencyInjection
+{
+    internal class EmployeeBAL
+    {
+        public IEmployeeDAL employeeDAL;
+
+        public EmployeeBAL(IEmployeeDAL employeeDAL)
+        {
+            this.employeeDAL = employeeDAL; 
+        }
+
+
+        public List<EmployeeData> GetAllEmmployees()
+        {
+            return employeeDAL.SelectAllEmployess();    
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using ConstructorDependencyInjection;
+
+EmployeeBAL employeeBAL = new EmployeeBAL(new EmployeeDAL());
+
+List<EmployeeData> ListEmployee = employeeBAL.GetAllEmmployees();
+foreach (EmployeeData emp in ListEmployee)
+{
+    Console.WriteLine("ID = {0}, Name = {1}, Department = {2}", emp.ID, emp.Name, emp.Department);
+}
+Console.ReadKey();
