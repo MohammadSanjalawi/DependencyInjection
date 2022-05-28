dff5b10e0b609836f0b736c89817571991013bc8
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-05-28 03:57:07 +0300

    Method Dependency Injection

---
+++ MethodDependencyInjection/EmployeeBAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDependencyInjection
+{
+    internal class EmployeeBAL
+    {
+        public IEmployeeDAL employeeDAL;
+
+        public List<EmployeeData> GetAllEmmployees(IEmployeeDAL employeeDAL)
+        {
+            this.employeeDAL = employeeDAL; 
+            return employeeDAL.SelectAllEmployess();
+        }
+    }
+}
---
+++ MethodDependencyInjection/Program.cs
@@ -0,0 +1,11 @@
+﻿using MethodDependencyInjection;
+
+EmployeeBAL employeeBAL = new EmployeeBAL();
+
+List<EmployeeData> ListEmployee = employeeBAL.GetAllEmmployees(new EmployeeDAL());
+
+foreach (EmployeeData emp in ListEmployee)
+{
+    Console.WriteLine("ID = {0}, Name = {1}, Department = {2}", emp.ID, emp.Name, emp.Department);
+}
+Console.ReadKey();
